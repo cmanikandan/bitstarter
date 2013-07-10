88fa0093805bc7a4b26c854d06dc18969f1bf6b3
Mani Chandrasekaran <mani.chandra@gmail.com>
2013-07-10 08:47:20 +0000

    made changes to read from index.html

--- web.js
+++ web.js
@@ -5,16 +5,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var filename = "index.html";
-  //response.send('Hello World 2!');
-  new BufferedReader (filename, { encoding: "utf8" })
-    .on ("error", function (error){
-        console.log ("error: " + error);
-    })
-    .on ("line", function (line){
-        response.send(line);
-    })
-    .read ();
-});
+  fs.readFile(filename,"utf8", function(error, data){
+    response.send(data);
+  });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
